d5af7cef52ab219097d0542d05e387ce9146218a
EveÃlyn21-cyber <evelyn.santos@aluno.imepac.edu.br>
2025-02-27 21:41:30 -0300

    MÂétodo par e impar

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner read = new Scanner(System.in);
+        System.out.printf("Informe um número");
+        int num = read.nextInt();
+
+        if (num % 2 == 0){
+            System.out.println("O número é " + num + " par ");
+        }else {
+            System.out.println("O número é " + num + " impar ");
+        }
+
+
         }
     }
-}
