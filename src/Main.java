c521645925abf46f69a4deeba7694c7dcbd14de0
EveÃlyn21-cyber <evelyn.santos@aluno.imepac.edu.br>
2025-02-27 21:08:53 -0300

    CriaÃção da Soma

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        }
+    public static void main(String[] args){
+        int num1 = 5;
+        int num2 = 2;
+        int result = (num1 + num2);
+        System.out.println(num1 + "+"  + num2 + "=" +result);
+
     }
-}
+
+
+        }
