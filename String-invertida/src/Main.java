8c02bf148fa86c322e1fbeef30d4049e89fc18db
Evelyn21-cyber <evelyn.santos@aluno.imepac.edu.br>
2025-03-06 12:47:10 -0300

    Palavras Invertidas

--- String-invertida/src/Main.java
+++ String-invertida/src/Main.java
@@ -15,7 +15,8 @@ public class Main {
         }
         System.out.println("Palavra original: " + word);
         System.out.println("Palavra invertida: " + wordreverse);
+    }
+    {
 
-        }
     }
 }
